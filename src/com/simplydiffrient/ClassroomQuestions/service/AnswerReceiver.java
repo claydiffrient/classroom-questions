41e76d848ded162b9f3de83ab9cbab941f969516
Clay Diffrient <clay.diffrient@gmail.com>
2014-04-09 18:03:54 -0600

    Adds JavaDoc comments to the AnswerReceiver

--- src/com/simplydiffrient/ClassroomQuestions/service/AnswerReceiver.java
+++ src/com/simplydiffrient/ClassroomQuestions/service/AnswerReceiver.java
@@ -1,42 +1,73 @@
 package com.simplydiffrient.ClassroomQuestions.service;
 
-import com.simplydiffrient.ClassroomQuestions.system.MulticastHandler;
 import com.simplydiffrient.ClassroomQuestions.system.UnicastHandler;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-import java.net.InetAddress;
 
 /**
- * Created by clay on 4/5/14.
+ * AnswerReceiver
+ *
+ * @author Clay Diffrient (claydiffrient)
+ * @version 1.0.0
+ *
+ * This class handles receiving answers from the various students.
  */
 public class AnswerReceiver
 {
+    /**
+     * List which holds the answers received.
+     */
     ObservableList<Character> mRawAnswersReceived;
+
+    /**
+     * Handler for all the unicast type operations.
+     * @see com.simplydiffrient.ClassroomQuestions.system.UnicastHandler
+     */
     UnicastHandler mHandler;
 
+    /**
+     * Default constructor
+     */
     public AnswerReceiver()
     {
         mRawAnswersReceived = FXCollections.observableArrayList();
         mHandler = new UnicastHandler(mRawAnswersReceived);
-        getResponses();
     }
 
+    /**
+     * Getter for the answers contained in the list.
+     * @return The list of received answers
+     */
     public ObservableList<Character> getResponses()
     {
         return mRawAnswersReceived;
     }
 
+    /**
+     * Wrapper for the listen method in the underlying handler.
+     * Starts listening for responses.
+     * @see com.simplydiffrient.ClassroomQuestions.system.UnicastHandler#listen()
+     */
     public void listen()
     {
         mHandler.listen();
     }
 
+    /**
+     * Tells the handler to stop listening.
+     * @see com.simplydiffrient.ClassroomQuestions.system.UnicastHandler#setListening(boolean)
+     */
     public void stopListening()
     {
         mHandler.setListening(false);
     }
 
+    /**
+     * Tells the handler to start listening.
+     * Note this is only needed if it was stopped previously.
+     * @see com.simplydiffrient.ClassroomQuestions.system.UnicastHandler#setListening(boolean)
+     */
     public void startListening()
     {
         mHandler.setListening(true);
